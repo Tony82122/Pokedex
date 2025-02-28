5619ca0045109e2d1a0e6bb54ac0bbda71afa139
Tony82122 <143779031+Tony82122@users.noreply.github.com>
2025-02-28 22:01:38 +0100

    hello

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,67 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 function Pokedex() {
-  const [pokemon, setPokemon] = useState([]);
-  const [currentPage, setCurrentPage] = useState('https://pokeapi.co/api/v2/pokemon');
-  const [nextPage, setNextPage] = useState();
-  const [prevPage, setPrevPage] = useState();
-  const [loading, setLoading] = useState(true);
-  const [selectedPokemon, setSelectedPokemon] = useState(null);
+    const [pokemon, setPokemon] = useState([]);
+    const [currentPage, setCurrentPage] = useState('https://pokeapi.co/api/v2/pokemon');
+    const [nextPage, setNextPage] = useState();
+    const [prevPage, setPrevPage] = useState();
+    const [loading, setLoading] = useState(true);
+    const [selectedPokemon, setSelectedPokemon] = useState(null);
 
-  useEffect(() => {
-    setLoading(true);
-    let cancel;
-    axios.get(currentPage, {
-      cancelToken: new axios.CancelToken(c => cancel = c)
-    }).then(res => {
-      setLoading(false);
-      setNextPage(res.data.next);
-      setPrevPage(res.data.previous);
-      setPokemon(res.data.results.map(p => p.name));
-    })
+    useEffect(() => {
+        setLoading(true);
+        let cancel;
+        axios.get(currentPage, {
+            cancelToken: new axios.CancelToken(c => cancel = c)
+        }).then(res => {
+            setLoading(false);
+            setNextPage(res.data.next);
+            setPrevPage(res.data.previous);
+            setPokemon(res.data.results.map(p => p.name));
+        })
 
-    return () => cancel()
-  }, [currentPage])
+        return () => cancel()
+    }, [currentPage])
 
-  function gotoNextPage() {
-    setCurrentPage(nextPage);
-  }
+    function gotoNextPage() {
+        setCurrentPage(nextPage);
+    }
 
-  function gotoPrevPage() {
-    setCurrentPage(prevPage);
-  }
+    function gotoPrevPage() {
+        setCurrentPage(prevPage);
+    }
 
-  async function selectPokemon(name) {
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    setSelectedPokemon(res.data);
-  }
+    async function selectPokemon(name) {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        setSelectedPokemon(res.data);
+    }
 
-  if (loading) return "Loading..."
+    if (loading) return "Loading..."
 
-  return (
-    <>
-      <div>
-        {pokemon.map(p => (
-          <button key={p} onClick={() => selectPokemon(p)}>{p}</button>
-        ))}
-      </div>
-      <div>
-        {prevPage && <button onClick={gotoPrevPage}>Previous</button>}
-        {nextPage && <button onClick={gotoNextPage}>Next</button>}
-      </div>
-      {selectedPokemon && (
-        <div>
-          <h2>{selectedPokemon.name}</h2>
-          <img src={selectedPokemon.sprites.front_default} alt={selectedPokemon.name} />
-          <p>Height: {selectedPokemon.height}</p>
-          <p>Weight: {selectedPokemon.weight}</p>
-          <p>Types: {selectedPokemon.types.map(t => t.type.name).join(', ')}</p>
-          <p>Abilities: {selectedPokemon.abilities.map(a => a.ability.name).join(', ')}</p>
-        </div>
-      )}
-    </>
-  )
+    return (
+        <>
+            <div>
+                {pokemon.map(p => (
+                    <button key={p}
+                            onClick={() => selectPokemon(p)}>{p}</button>
+                ))}
+            </div>
+            <div>
+                {prevPage && <button onClick={gotoPrevPage}>Previous</button>}
+                {nextPage && <button onClick={gotoNextPage}>Next</button>}
+            </div>
+            {selectedPokemon && (
+                <div>
+                    <h2>{selectedPokemon.name}</h2>
+                    <img src={selectedPokemon.sprites.front_default}
+                         alt={selectedPokemon.name}/>
+                    <p>Height: {selectedPokemon.height}</p>
+                    <p>Weight: {selectedPokemon.weight}</p>
+                    <p>Types: {selectedPokemon.types.map(t => t.type.name).join(', ')}</p>
+                    <p>Abilities: {selectedPokemon.abilities.map(a => a.ability.name).join(', ')}</p>
+                </div>
+            )}
+        </>
+    )
 }
 
 export default Pokedex;
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function PokemonCard({ number, name, type, imageUrl }) {
-  return (
-    <div className={`pokemon-card ${type.toLowerCase()}`}>
-      <div>
-        <div className="number">#{number}</div>
-        <div>{name}</div>
-      </div>
-      <img src={imageUrl} alt={name} />
-    </div>
-  );
+function PokemonCard({number, name, type, imageUrl}) {
+    return (
+        <div className={`pokemon-card ${type.toLowerCase()}`}>
+            <div>
+                <div className="number">#{number}</div>
+                <div>{name}</div>
+            </div>
+            <img src={imageUrl} alt={name}/>
+        </div>
+    );
 }
 
 export default PokemonCard;
