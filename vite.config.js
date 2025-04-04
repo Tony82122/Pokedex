86abf06bc500b6adc22fef38d72780481ab43878
Tony82122 <143779031+Tony82122@users.noreply.github.com>
2025-04-04 20:51:24 +0200

    fixes in code and stats , also vite issue

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    port: 3000,
-  },
-});
+})
