6297982adb2c51de8052b0757455486a40de80cf
Tony82122 <143779031+Tony82122@users.noreply.github.com>
2025-04-22 12:19:37 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,83 @@
 import React, { useState } from 'react';
-import axios from 'axios';  // Add this import
+import axios from 'axios';
 import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 
-// This defines the App structure
-
-const styles  = {
+const styles = {
     body: {
-        fontFamily: 'Arial, sans-serif',
+        fontFamily: "'Roboto', sans-serif",
         margin: 0,
         padding: 0,
-        backgroundColor: '#eeeaea',
-        borderRadius : '50px',
+        backgroundColor: '#f0f0f0',
+        minHeight: '100vh',
     },
     app: {
         maxWidth: '1200px',
         margin: '0 auto',
         padding: '20px',
     },
+    header: {
+        backgroundColor: '#3c5aa6',
+        color: 'white',
+        padding: '20px 0',
+        textAlign: 'center',
+        boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+    },
+    title: {
+        margin: 0,
+        fontSize: '2.5em',
+        textShadow: '2px 2px 4px rgba(0,0,0,0.3)',
+    },
+    nav: {
+        backgroundColor: '#2a75bb',
+        padding: '10px 0',
+    },
+    navList: {
+        listStyle: 'none',
+        padding: 0,
+        margin: 0,
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    navItem: {
+        margin: '0 15px',
+    },
+    navLink: {
+        color: 'white',
+        textDecoration: 'none',
+        fontSize: '1.1em',
+        transition: 'color 0.3s ease',
+    },
     pokemonGrid: {
         display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
         gap: '20px',
+        padding: '20px 0',
     },
     pokemonCard: {
         borderRadius: '10px',
         padding: '20px',
         display: 'flex',
-        justifyContent: 'space-between',
+        flexDirection: 'column',
         alignItems: 'center',
         color: 'white',
-        fontSize: '24px',
+        fontSize: '18px',
         fontWeight: 'bold',
         boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
         cursor: 'pointer',
+        transition: 'transform 0.3s ease, box-shadow 0.3s ease',
     },
     number: {
         fontSize: '14px',
         opacity: 0.7,
+        marginBottom: '5px',
     },
     image: {
-        maxWidth: '100px',
-        maxHeight: '100px',
+        maxWidth: '120px',
+        maxHeight: '120px',
+        marginBottom: '10px',
+        transition: 'transform 0.3s ease',
     },
     modal: {
         position: 'fixed',
@@ -64,31 +99,48 @@ const styles  = {
     },
     modalContent: {
         backgroundColor: 'white',
-        padding: '20px',
-        borderRadius: '10px',
-        maxWidth: '400px',
-        width: '100%',
-        overflowY: 'auto',
+        padding: '30px',
+        borderRadius: '15px',
+        maxWidth: '500px',
+        width: '90%',
         maxHeight: '80vh',
+        overflowY: 'auto',
         transform: 'scale(0.7)',
         opacity: 0,
         transition: 'transform 0.3s ease, opacity 0.3s ease',
+        boxShadow: '0 5px 15px rgba(0,0,0,0.3)',
     },
     modalContentVisible: {
         transform: 'scale(1)',
         opacity: 1,
     },
     closeButton: {
-        float: 'right',
+        position: 'absolute',
+        top: '10px',
+        right: '15px',
         cursor: 'pointer',
-        fontSize: '20px',
+        fontSize: '24px',
+        color: '#333',
     },
     statsList: {
         listStyleType: 'none',
         padding: 0,
     },
     statItem: {
-        marginBottom: '5px',
+        marginBottom: '10px',
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    statBar: {
+        height: '10px',
+        backgroundColor: '#e0e0e0',
+        borderRadius: '5px',
+        overflow: 'hidden',
+    },
+    statBarFill: {
+        height: '100%',
+        backgroundColor: '#4CAF50',
+        transition: 'width 0.5s ease-out',
     },
 };
 
@@ -312,84 +364,89 @@ function App() {
 
     return (
         <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
+            <div style={styles.body}>
+                <header style={styles.header}>
+                    <h1 style={styles.title}>PokeMalarkey</h1>
+                </header>
+                <nav style={styles.nav}>
+                    <ul style={styles.navList}>
+                        <li style={styles.navItem}>
+                            <Link to="/" style={styles.navLink}>Home</Link>
                         </li>
-                        <li>
-                            <Link to="/about">About</Link>
+                        <li style={styles.navItem}>
+                            <Link to="/about" style={styles.navLink}>About</Link>
                         </li>
                     </ul>
                 </nav>
 
                 <Routes>
                     <Route path="/" element={
-                        <div style={styles.body}>
-                            <div style={styles.app}>
-                                <h1>PokeMalarkey</h1>
-                                <div style={styles.pokemonGrid}>
-                                    {pokemonData.map((pokemon) => (
-                                        <div
-                                            key={pokemon.number}
-                                            style={{
-                                                ...styles.pokemonCard,
-                                                backgroundColor: typeColors[pokemon.type.toLowerCase()],
-                                            }}
-                                            onClick={() => handlePokemonClick(pokemon)}
-                                        >
-                                            <div>
-                                                <div
-                                                    style={styles.number}>#{pokemon.number}</div>
-                                                <div>{pokemon.name}</div>
-                                            </div>
-                                            <img src={pokemon.imageUrl} alt={pokemon.name}
-                                                 style={styles.image}/>
-                                        </div>
-                                    ))}
-                                </div>
-                            </div>
-                            <div 
-                                style={{
-                                    ...styles.modal,
-                                    ...(isModalVisible ? styles.modalVisible : {})
-                                }} 
-                                onClick={closeModal}
-                            >
-                                <div 
-                                    style={{
-                                        ...styles.modalContent,
-                                        ...(isModalVisible ? styles.modalContentVisible : {})
-                                    }} 
-                                    onClick={(e) => e.stopPropagation()}
-                                >
-                                    <span style={styles.closeButton} onClick={closeModal}>&times;</span>
-                                    {loading ? (
-                                        <p>Loading...</p>
-                                    ) : selectedPokemon && (
-                                        <>
-                                            <h2>{selectedPokemon.name}</h2>
-                                            <p>Number: {selectedPokemon.number}</p>
-                                            <p>Type: {selectedPokemon.type}</p>
-                                            <img src={selectedPokemon.imageUrl} alt={selectedPokemon.name} style={styles.image}/>
-                                            <p>Height: {selectedPokemon.height}</p>
-                                            <p>Weight: {selectedPokemon.weight}</p>
-                                            <p>Abilities: {selectedPokemon.abilities.join(', ')}</p>
-                                            <h3>Stats:</h3>
-                                            <ul style={styles.statsList}>
-                                                {selectedPokemon.stats.map((stat, index) => (
-                                                    <li key={index} style={styles.statItem}>{stat.name}: {stat.value}</li>
-                                                ))}
-                                            </ul>
-                                        </>
-                                    )}
-                                </div>
+                        <div style={styles.app}>
+                            <div style={styles.pokemonGrid}>
+                                {pokemonData.map((pokemon) => (
+                                    <div
+                                        key={pokemon.number}
+                                        style={{
+                                            ...styles.pokemonCard,
+                                            backgroundColor: typeColors[pokemon.type.toLowerCase()],
+                                        }}
+                                        onClick={() => handlePokemonClick(pokemon)}
+                                    >
+                                        <div style={styles.number}>#{pokemon.number}</div>
+                                        <img src={pokemon.imageUrl} alt={pokemon.name} style={styles.image}/>
+                                        <div>{pokemon.name}</div>
+                                    </div>
+                                ))}
                             </div>
                         </div>
                     } />
                     <Route path="/about" element={<About />} />
                 </Routes>
+
+                <div 
+                    style={{
+                        ...styles.modal,
+                        ...(isModalVisible ? styles.modalVisible : {})
+                    }} 
+                    onClick={closeModal}
+                >
+                    <div 
+                        style={{
+                            ...styles.modalContent,
+                            ...(isModalVisible ? styles.modalContentVisible : {})
+                        }} 
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        <span style={styles.closeButton} onClick={closeModal}>&times;</span>
+                        {loading ? (
+                            <p>Loading...</p>
+                        ) : selectedPokemon && (
+                            <>
+                                <h2>{selectedPokemon.name}</h2>
+                                <p>Number: {selectedPokemon.number}</p>
+                                <p>Type: {selectedPokemon.type}</p>
+                                <img src={selectedPokemon.imageUrl} alt={selectedPokemon.name} style={styles.image}/>
+                                <p>Height: {selectedPokemon.height}</p>
+                                <p>Weight: {selectedPokemon.weight}</p>
+                                <p>Abilities: {selectedPokemon.abilities.join(', ')}</p>
+                                <h3>Stats:</h3>
+                                <ul style={styles.statsList}>
+                                    {selectedPokemon.stats.map((stat, index) => (
+                                        <li key={index} style={styles.statItem}>
+                                            <span>{stat.name}: {stat.value}</span>
+                                            <div style={styles.statBar}>
+                                                <div style={{
+                                                    ...styles.statBarFill,
+                                                    width: `${(stat.value / 255) * 100}%`
+                                                }}></div>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </>
+                        )}
+                    </div>
+                </div>
             </div>
         </Router>
     );
