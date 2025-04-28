794cd4dfb43565c121692ee8234731e92002c72b
Tony82122 <143779031+Tony82122@users.noreply.github.com>
2025-04-28 13:23:30 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './Pages/Home';
@@ -142,6 +142,12 @@ const styles = {
         backgroundColor: '#4CAF50',
         transition: 'width 0.5s ease-out',
     },
+    pagination: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '20px',
+    },
 };
 
 const typeColors = {
@@ -169,171 +175,41 @@ function App() {
     const [selectedPokemon, setSelectedPokemon] = useState(null);
     const [loading, setLoading] = useState(false);
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [pokemonData, setPokemonData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
+    const pokemonPerPage = 20;
 
-    const pokemonData = [
-        {
-            number: '1',
-            name: 'Bulbasaur',
-            type: 'Grass',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-        },
-        {
-            number: '2',
-            name: 'Ivysaur',
-            type: 'Grass',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png'
-        },
-        {
-            number: '3',
-            name: 'Venusaur',
-            type: 'Grass',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png'
-        },
-        {
-            number: '4',
-            name: 'Charmander',
-            type: 'Fire',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
-        },
-        {
-            number: '5',
-            name: 'Charmeleon',
-            type: 'Fire',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png'
-        },
-        {
-            number: '6',
-            name: 'Charizard',
-            type: 'Fire',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'
-        },
-        {
-            number: '7',
-            name: 'Squirtle',
-            type: 'Water',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png'
-        },
-        {
-            number: '8',
-            name: 'Wartortle',
-            type: 'Water',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png'
-        },
-        {
-            number: '9',
-            name: 'Blastoise',
-            type: 'Water',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png'
-        },
-        {
-            number: '10',
-            name: 'Caterpie',
-            type: 'Bug',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png'
-        },
-        {
-            number: '11',
-            name: 'Metapod',
-            type: 'Bug',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png'
-        },
-        {
-            number: '12',
-            name: 'Butterfree',
-            type: 'Bug',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png'
-        },
-        {
-            number: '13',
-            name: 'Weedle',
-            type: 'Bug',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png'
-        },
-        {
-            number: '14',
-            name: 'Kakuna',
-            type: 'Bug',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/14.png'
-        },
-        {
-            number: '15',
-            name: 'Beedrill',
-            type: 'Bug',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png'
-        },
-        {
-            number: '16',
-            name: 'Pidgey',
-            type: 'Normal',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png'
-        },
-        {
-            number: '17',
-            name: 'Pidgeotto',
-            type: 'Normal',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png'
-        },
-        {
-            number: '18',
-            name: 'Pidgeot',
-            type: 'Normal',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/18.png'
-        },
-        {
-            number: '19',
-            name: 'Rattata',
-            type: 'Normal',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png'
-        },
-        {
-            number: '20',
-            name: 'Raticate',
-            type: 'Normal',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png'
-        },
-        {
-            number: '21',
-            name: 'Spearow',
-            type: 'Normal',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/21.png'
-        },
-        {
-            number: '22',
-            name: 'Fearow',
-            type: 'Normal',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/22.png'
-        },
-        {
-            number: '23',
-            name: 'Ekans',
-            type: 'Poison',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/23.png'
-        },
-        {
-            number: '24',
-            name: 'Arbok',
-            type: 'Poison',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/24.png'
-        },
-        {
-            number: '25',
-            name: 'Pikachu',
-            type: 'Electric',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'
-        },
-        {
-            number: '26',
-            name: 'Raichu',
-            type: 'Electric',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/26.png'
-        },
-        {
-            number: '27',
-            name: 'Sandshrew',
-            type: 'Ground',
-            imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/27.png'
-        },
-    ];
+    useEffect(() => {
+        fetchPokemon();
+    }, [currentPage]);
+
+    const fetchPokemon = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0`);
+            const results = response.data.results;
+            setTotalPages(Math.ceil(results.length / pokemonPerPage));
+
+            const detailedPokemon = await Promise.all(
+                results.slice((currentPage - 1) * pokemonPerPage, currentPage * pokemonPerPage)
+                    .map(async (pokemon, index) => {
+                        const detailResponse = await axios.get(pokemon.url);
+                        return {
+                            number: String(detailResponse.data.id).padStart(3, '0'),
+                            name: detailResponse.data.name,
+                            type: detailResponse.data.types[0].type.name,
+                            imageUrl: detailResponse.data.sprites.front_default
+                        };
+                    })
+            );
+
+            setPokemonData(detailedPokemon);
+        } catch (error) {
+            console.error("Error fetching Pokemon:", error);
+        }
+        setLoading(false);
+    };
 
     const handlePokemonClick = async (pokemon) => {
         setLoading(true);
@@ -359,7 +235,11 @@ function App() {
 
     const closeModal = () => {
         setIsModalVisible(false);
-        setTimeout(() => setSelectedPokemon(null), 300); // Wait for transition to finish
+        setTimeout(() => setSelectedPokemon(null), 300);
+    };
+
+    const changePage = (newPage) => {
+        setCurrentPage(newPage);
     };
 
     return (
@@ -382,22 +262,43 @@ function App() {
                 <Routes>
                     <Route path="/" element={
                         <div style={styles.app}>
-                            <div style={styles.pokemonGrid}>
-                                {pokemonData.map((pokemon) => (
-                                    <div
-                                        key={pokemon.number}
-                                        style={{
-                                            ...styles.pokemonCard,
-                                            backgroundColor: typeColors[pokemon.type.toLowerCase()],
-                                        }}
-                                        onClick={() => handlePokemonClick(pokemon)}
-                                    >
-                                        <div style={styles.number}>#{pokemon.number}</div>
-                                        <img src={pokemon.imageUrl} alt={pokemon.name} style={styles.image}/>
-                                        <div>{pokemon.name}</div>
+                            {loading ? (
+                                <p>Loading...</p>
+                            ) : (
+                                <>
+                                    <div style={styles.pokemonGrid}>
+                                        {pokemonData.map((pokemon) => (
+                                            <div
+                                                key={pokemon.number}
+                                                style={{
+                                                    ...styles.pokemonCard,
+                                                    backgroundColor: typeColors[pokemon.type.toLowerCase()],
+                                                }}
+                                                onClick={() => handlePokemonClick(pokemon)}
+                                            >
+                                                <div style={styles.number}>#{pokemon.number}</div>
+                                                <img src={pokemon.imageUrl} alt={pokemon.name} style={styles.image}/>
+                                                <div>{pokemon.name}</div>
+                                            </div>
+                                        ))}
                                     </div>
-                                ))}
-                            </div>
+                                    <div style={styles.pagination}>
+                                        <button 
+                                            onClick={() => changePage(currentPage - 1)} 
+                                            disabled={currentPage === 1}
+                                        >
+                                            Previous
+                                        </button>
+                                        <span>{currentPage} of {totalPages}</span>
+                                        <button 
+                                            onClick={() => changePage(currentPage + 1)} 
+                                            disabled={currentPage === totalPages}
+                                        >
+                                            Next
+                                        </button>
+                                    </div>
+                                </>
+                            )}
                         </div>
                     } />
                     <Route path="/about" element={<About />} />
